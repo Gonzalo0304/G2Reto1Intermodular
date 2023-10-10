23339335645776e9992cd907f71f543bef0e0df0
Iñigo <>
2023-10-10 13:05:33 +0200

    Commit Inicial Cliente

---
+++ Reto2Cliente/src/Reto2Servidor.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Iñigo
+ */
+public class Reto2Servidor {
+    
+}
