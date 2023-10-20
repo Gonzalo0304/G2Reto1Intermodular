fd9911ec0c6390451720a6c2f69547e42913c595
David <2dam@HZ359575.TartangaLH.eus>
2023-10-20 08:36:12 +0200

    Avances.

--- Reto2Cliente/src/controller/ControllerSignUp.java
+++ Reto2Cliente/src/controller/ControllerSignUp.java
@@ -39,6 +39,12 @@ import javafx.stage.WindowEvent;
 import modelo.Implementacion;
 import modelo.InterfazCliente;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 /**
  *
  * @author David, Gonzalo.
